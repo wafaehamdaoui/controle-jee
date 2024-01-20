b0b864be10a9886f0c7d192ebc32612f84e2a19d
wafaehamdaoui <hamdaouiwafae2000@gmail.com>
2024-01-20 12:32:06 +0100

    commit 2

--- src/main/java/com/eidia/newtp/controller/ProduitController.java
+++ src/main/java/com/eidia/newtp/controller/ProduitController.java
@@ -2,7 +2,6 @@ package com.eidia.newtp.controller;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
